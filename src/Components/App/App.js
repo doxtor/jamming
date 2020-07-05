8fc5464a12145f9be5495e9a92c9a76c60369c9b
doxtor <anugroho.daniel@gmail.com>
2020-07-05 20:03:01 +0800

    add tracks to playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -16,13 +16,25 @@ class App extends React.Component {
       ],
       playlistName: 'My Playlist',
       playlistTracks: [
-        {name: 'playlist name', artist: 'fave artist', album: 'fave album', id: 1},
-        {name: 'playlist name2', artist: 'fave artist2', album: 'fave album2', id: 2},
-        {name: 'playlist name3', artist: 'fave artist3', album: 'fave album3', id: 3}
+        {name: 'playlist name', artist: 'fave artist', album: 'fave album', id: 4},
+        {name: 'playlist name2', artist: 'fave artist2', album: 'fave album2', id: 5},
+        {name: 'playlist name3', artist: 'fave artist3', album: 'fave album3', id: 6}
       ]
-    }
+    };
+
+    this.addTrack = this.addTrack.bind(this);
 
   }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+        return;
+      }
+    tracks.push(track);  
+    this.setState({playlistTracks: tracks});
+  }
+
   render() {
     return (
       <div>
@@ -30,7 +42,7 @@ class App extends React.Component {
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
           <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
         </div>
       </div>
